73bffcaf80905a8931ee9bdf43ae03cc50838868
Joshua Bonilla <joshua.bonilla@upr.edu>
2018-02-25 18:48:30 -0400

    Area calculation

---
+++ CirclaAreaCalculatorMain.java
@@ -0,0 +1,12 @@
+
+public class CirclaAreaCalculatorMain {
+	static int radious;
+	static double area;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		radious = 3;
+		area = 2*Math.PI*Math.pow(radious, 2);
+		System.out.println("The area of a circle with radious "+radious+" is: "+area);
+	}
+
+}
